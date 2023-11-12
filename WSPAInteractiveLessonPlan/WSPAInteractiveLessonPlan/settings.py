7b9fadb3689286707869756d0227c4509285a33e
Klegus <przemek19r@gmail.com>
2023-11-12 14:12:56 +0100

    railway-prep

--- WSPAInteractiveLessonPlan/WSPAInteractiveLessonPlan/settings.py
+++ WSPAInteractiveLessonPlan/WSPAInteractiveLessonPlan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@bqqptkla4g1i3%wp2@95#&%ce=-6-6-x22=))y^^#89p69mr-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
