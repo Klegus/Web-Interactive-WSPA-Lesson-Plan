f4d6ea3e78d2a9c0bd6216b8bdc7fc14dd73c79c
Klegus <31622223+Klegus@users.noreply.github.com>
2024-10-05 00:01:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def get_comparisons(group_name: str):
 async def get_status():
     try:
         async with httpx.AsyncClient() as client:
-            response = await client.get("http://3.78.248.208/status")
+            response = await client.get("http://37.27.207.141/status")
         
         if response.status_code == 200:
             return response.json()
