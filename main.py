3dfe8ef9c708fac17ff49689cdb2b1c13e1e584c
Klegus <przemek19r@gmail.com>
2024-11-05 19:01:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -153,6 +153,6 @@ async def get_status():
 if DEV == 'True':
     if __name__ == "__main__":
         import uvicorn
-        uvicorn.run(app, host="127.0.0.1", port=8000)
+        uvicorn.run(app, host="127.0.0.1", port=80)
 else:
     handler = Mangum(app)
